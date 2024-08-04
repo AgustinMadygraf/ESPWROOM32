7de595619f619caf9bd534cd4f6e781d742dad19
AgustinMadygraf <agustin.mtto.madygraf>
2024-08-04 20:36:14 -0300

    Refactor database configuration and remove unused code

--- ESP32Project/config.py
+++
@@ -1,5 +0,0 @@
-#ESPWROOM32/ESP32Project/config.py
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://tu_usuario:tu_contraseña@localhost/nombre_de_tu_base_de_datos'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
