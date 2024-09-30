9dd584826eb31c12857e08ae7c14cc20599ecbda
AgustinMadygraf <agustin.mtto.madygraf@gmail.com>
2024-09-30 18:05:25 -0300

    Refactor file paths, update documentation, and add equipment failure analysis and maintenance plan

--- app/views/mantenimiento.php
+++ app/views/mantenimiento.php
@@ -1,8 +1,28 @@
 <?php
-// index.php
-require 'header.php';
-?>
+// app/views/mantenimiento.php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
+require 'header.php';
+
+// Verificar si el archivo autoload.php existe
+if (!file_exists('../../vendor/autoload.php')) {
+    die('Error: No se encontró el archivo autoload.php');
+}
+
+require '../../vendor/autoload.php';
+
+$parsedown = new Parsedown();
+
+// Verificar si el archivo mantenimiento.md existe
+if (!file_exists('../../docs/mantenimiento.md')) {
+    die('Error: No se encontró el archivo mantenimiento.md');
+}
+
+$markdownContent = file_get_contents('../../docs/mantenimiento.md');
+$htmlContent = $parsedown->text($markdownContent);
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -16,16 +36,10 @@ require 'header.php';
         <h1>Plan de Mantenimiento</h1>
     </header>
     <main>
-    <h4>El mantenimiento Industrial es el conjunto de acciones que se realizan para mantener en buen estado las instalaciones, 
-    maquinaria y equipos de una empresa.<br>
-    El listado de equipos y maquinaria que requieren mantenimiento se puede obtener de la base de datos.<br>
-    La definición de fallo técnico son aquellas situaciones en las que un equipo o maquinaria no funciona correctamente.<br>
-    </h4>
-    <div style="text-align: center;">
-        <img src="../../public/images/mantenimiento.jpeg" alt="Plan de Mantenimiento">
-    </div>
+        <?= $htmlContent ?>
     </main>
     <footer>
         <p>© 2024 madygraf</p>
     </footer>
 </body>
+</html>
