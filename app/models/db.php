c46c8489ce07c5f42251b7c6dfdeaed8c1ec9a66
AgustinMadygraf <agustin.mtto.madygraf>
2024-10-26 18:59:04 -0300

    Update file paths for autoload.php in db.php and procesar.php

--- app/models/db.php
+++ app/models/db.php
@@ -1,7 +1,7 @@
 <?php
 // automatizacion/app/models/db.php
 
-require_once '../../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 use Dotenv\Dotenv;
 
--- procesar.php
+++ procesar.php
@@ -1,11 +1,17 @@
 <?php
-require '../vendor/autoload.php'; // Cargar Composer y phpdotenv
-include 'app/models/db.php';
-ECHO "Carga de datos<br>";
+// automatizacion/procesar.php
+
+// Ajuste de ruta para asegurar que 'autoload.php' se carga desde la raíz del proyecto
+require_once __DIR__ . '/vendor/autoload.php'; 
+require_once __DIR__ . '/app/models/db.php';
+
+echo "Carga de datos<br>";
+
 // Verificar si los parámetros están presentes
 if (isset($_GET['balanza']) && isset($_GET['contador'])) {
     $balanza = $_GET['balanza'];
     $contador = $_GET['contador'];
+
     // Preparar y ejecutar la consulta SQL
     $sql = "INSERT INTO measurements (balanza, contador) VALUES ('$balanza', '$contador')";
     if ($conn->query($sql) === TRUE) {
@@ -18,5 +24,6 @@ if (isset($_GET['balanza']) && isset($_GET['contador'])) {
 } else {
     echo "Faltan parámetros";
 }
+
 // Cerrar conexión
 $conn->close();
