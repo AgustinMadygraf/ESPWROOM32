821942d8381e3c2431e78fc1c4e220aeeb3f0394
AgustinMadygraf <agustin.mtto.madygraf>
2024-10-27 18:37:49 -0300

    Refactor Vue app to improve template readability and update asset imports

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,22 @@
 const Encore = require('@symfony/webpack-encore');
 
 Encore
+    // Directorio donde se generarán los archivos compilados
     .setOutputPath('public/build/')
+    // Ruta pública utilizada por Symfony para acceder a los archivos
     .setPublicPath('/build')
+    // Habilitar Single Runtime Chunk
+    .enableSingleRuntimeChunk()
+    // Otras configuraciones de Encore...
     .addEntry('app', './assets/js/app.js')
     .addStyleEntry('global', './assets/css/global.scss')
-    .enableVueLoader()
-    .enableSingleRuntimeChunk()
-    .cleanupOutputBeforeBuild()
-    .enableSourceMaps(!Encore.isProduction())
-    .enableVersioning(Encore.isProduction())
+    // Habilitar Sass/SCSS
     .enableSassLoader()
-    .autoProvidejQuery();
+    // Habilitar la generación de archivos de fuente
+    .enableSourceMaps(!Encore.isProduction())
+    // Habilitar la versión de archivos (hashing)
+    .enableVersioning(Encore.isProduction())
+    // Otras configuraciones adicionales...
+;
 
 module.exports = Encore.getWebpackConfig();
