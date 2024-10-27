8cf54256e7e5571a3abadabdbb84aef8f034fac6
AgustinMadygraf <agustin.mtto.madygraf>
2024-10-27 18:53:58 -0300

    Refactor Vue app to use Encore entry script tags for asset imports

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,15 @@
 const Encore = require('@symfony/webpack-encore');
 
 Encore
-    // Directorio donde se generarán los archivos compilados
     .setOutputPath('public/build/')
-    // Ruta pública utilizada por Symfony para acceder a los archivos
     .setPublicPath('/build')
-    // Habilitar Single Runtime Chunk
     .enableSingleRuntimeChunk()
-    // Otras configuraciones de Encore...
     .addEntry('app', './assets/js/app.js')
+    .addEntry('vue_app', './assets/js/vue_app.js') // Asegúrate de agregar esta línea
     .addStyleEntry('global', './assets/css/global.scss')
-    // Habilitar Sass/SCSS
     .enableSassLoader()
-    // Habilitar la generación de archivos de fuente
     .enableSourceMaps(!Encore.isProduction())
-    // Habilitar la versión de archivos (hashing)
     .enableVersioning(Encore.isProduction())
-    // Otras configuraciones adicionales...
 ;
 
 module.exports = Encore.getWebpackConfig();
