3828f4d7497a73b33f897ee1b4661e539b716d3b
SantiGamer 55 <santiagoluanvega@gmail.com>
2024-10-28 20:27:26 -0300

    Refactor file paths in electronica.php

--- app/views/electronica.php
+++ app/views/electronica.php
@@ -17,7 +17,30 @@ require 'header.php';
     </header>
     <main>
         <h3>
-            *trabajo de los de electronica*
+        IoT internet de las cosas
+es la conexion de dispositivos cotidianos a internet par que puedan comunicarse entre si recopilar datos y ser controlados a distancia, incluyen electrodomesticos inteligentes y sistemas de automatizacion en hogares.
+
+HX711
+
+es un amplificador y convertidor analogico-digital usado con celdas de carga para medir peso. 
+amplifica la señal de la selda de carga y la convierte en una señal digital que un microcontrolador puede leer.
+
+display LCD 16x2
+
+el display LCD 12c 16x2 es una pantalla que muestra 16 caracteres en 2 lineas y utiliza un interfaz 12c para simplificar la conexion y control con microcontroladores.
+
+fuente reguladora 24-5v 
+
+una fuente reguladora de 24 a 5v convierte una temsion de 24v en una salida de tension estable de 5v, para alimentar dispocitivos electronicos que necesita esa tension.
+
+Microcontroador ESP-wroom-32 
+
+este modulo incluye un microcontrolador de 32 bits con conectividad wi-fi y bluetooth, y es ampliamente utilizado en proyectos de internet de las cosas (Iot) y dispositivos inteligentes.
+
+ESP32-cam 
+
+es un modulo compacto que convina un microcontrolador ESP32 con una camara y wi-fi ideal para proyectos de vision por computadora e Iot. 
+
         </h3>
     </main>
 </body>
