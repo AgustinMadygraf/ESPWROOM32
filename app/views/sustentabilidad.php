480d7e64bb5623e4fd2535230252b90d13362662
SantiGamer 55 <santiagoluanvega@gmail.com>
2024-10-28 19:26:33 -0300

    Refactor file paths in sustentabilidad.php

--- app/views/sustentabilidad.php
+++ app/views/sustentabilidad.php
@@ -18,9 +18,27 @@ require 'header.php';
     <main>
         <p>
             La Sustentabilidad es la capacidad de mantenerse en el tiempo sin agotar los recursos naturales. 
-            En el caso de la industria, la sustentabilidad se refiere a la capacidad de producir bienes y servicios sin dañar el medio ambiente.
-        </p>
-    </main>
+            En el caso de la industria, la sustentabilidad se refiere a la capacidad de producir bienes y 
+            servicios sin dañar el medio ambiente.
+
+            la sustentabilidad:se refiere ala capacidad de una sociedad para desarollar y mantener un entorno 
+            saludable y equitativo para sus cuidadanos ahora y en el futuro
+            
+            la sustantibilidad ambiental: implica gestionar de manera responsable los recursos naturales para 
+            sastifacer necesidades actuales sin comprometer las de las futura generaciones 
+
+            La diferencia entre un monocultivo de arboles y bosque nativo
+
+            Monocultivo de arboles:consiste en la plantacion de una sola especie de arbol generalmente con fines
+            comerciales, como la produccion de madera, pulpa de papel o aceite
+
+            bosque nativo:Esta compuesto por una gran diversidad de especies de arboles, arbustos,platas,herbaceas,hongos 
+            y animales.
+            Las plantaciones industriales de arboles, al consumir grandes cantidades de agua y alterar el ciclo hidrologico 
+            pueden agravar las crisis hidricas al reducir las disponibilidad de agua en rios, arroyos y acuiferos afectando
+            las comunidades y ecosistemas locales.
+    
+        </p
     <footer>
         <p>© 2024 madygraf</p>
     </footer>
